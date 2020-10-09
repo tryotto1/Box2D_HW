7802219704257aca35a062fab1feaefd3ffb4735
tryotto <tryotto@naver.com>
2020-10-09 16:38:35 +0900

    1008

--- Box2D_Example/main.cpp
+++ Box2D_Example/main.cpp
@@ -19,13 +19,29 @@ b2Body* box;
 b2Body* pin_ball;
 b2Body* leftFlipper;
 b2Body* rightFlipper;
+b2Body* ball_shooter;	// 발사대 설정
+b2Body* scissor_1;		// 장애물 - 가위
+b2Body* scissor_2;
+b2Body* prisma_1;		// 장애물 - prismatic
+b2Body* prisma_2;
+b2Body* tmp_gnd;
+b2Body* tmp_gnd2;
 b2RevoluteJoint* m_leftJoint;	// Flipper 설정
 b2RevoluteJoint* m_rightJoint;
+b2RevoluteJoint* m_ball_shooter;	// 발사대 설정
+b2RevoluteJoint* m_scissor;
+b2RevoluteJoint* m_scissor_2;
+b2PrismaticJoint* m_prisma;
 
 // shape 설정
 b2ChainShape gnd_shape;
 b2CircleShape ballshape;
 b2PolygonShape flip_box;
+b2PolygonShape shooter_box;
+b2PolygonShape scissor_box;
+b2PolygonShape prisma_box;
+b2PolygonShape tmp_box;
+b2PolygonShape tmp_box2;
 
 int32 velocityIterations = 8;	//the number of iterations for computing the impulses
 int32 positionIterations = 3;	//the number of iterations for adjusting the position
@@ -36,7 +52,7 @@ float32 timeStep = 1.0f / g_hz;
 void Render()
 {
 	// (1) Initialize glut - 기본 꼴. 수정할 필요 전혀 없음
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(0.8f, 0.8f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glMatrixMode(GL_PROJECTION);
@@ -54,7 +70,7 @@ void Render()
 	glPushMatrix();
 	glTranslatef(position.x, position.y, 0.0f);
 	glRotatef(angle, 0.0f, 0.0f, 1.0f);			
-	glColor3f(5.9f, 5.2f, 5.4f);			   
+	glColor3f(0.1f, 0.1f, 0.1f);			   
 
 	glLineWidth(1.0f);
 	glBegin(GL_LINE_LOOP);	
@@ -89,7 +105,7 @@ void Render()
 
 	/* (4) flipper 그리기 - 왼쪽 */
 	position = leftFlipper->GetPosition();
-	angle = leftFlipper->GetAngle();
+	angle = leftFlipper->GetAngle() * 180 / b2_pi;
 
 	glPushMatrix();
 	glTranslatef(position.x, position.y, 0.0f);
@@ -101,11 +117,12 @@ void Render()
 		glVertex2f(flip_box.m_vertices[i].x, flip_box.m_vertices[i].y);
 	}
 	glEnd();
+	glFinish();
 	glPopMatrix();
 
 	/* (5) flipper 그리기 - 오른쪽 */
 	position = rightFlipper->GetPosition();
-	angle = rightFlipper->GetAngle();
+	angle = rightFlipper->GetAngle() * 180/b2_pi;
 
 	glPushMatrix();
 	glTranslatef(position.x, position.y, 0.0f);
@@ -117,9 +134,110 @@ void Render()
 		glVertex2f(flip_box.m_vertices[i].x, flip_box.m_vertices[i].y);
 	}
 	glEnd();
+	glFinish();
 	glPopMatrix();
 
-	glutSwapBuffers();		
+	/* (6) scissor 그리기 */
+	position = scissor_1->GetPosition();
+	angle = scissor_1->GetAngle() * 180 / b2_pi;
+
+	glPushMatrix();
+	glTranslatef(position.x, position.y, 0.0f);
+	glRotatef(angle, 0.0f, 0.0f, 1.0f);
+	glColor3f(0.5f, 0.1f, 1.0f);
+
+	glBegin(GL_QUADS);
+	for (int i = 0; i < 4; i++) {
+		glVertex2f(scissor_box.m_vertices[i].x, scissor_box.m_vertices[i].y);
+	}
+	glEnd();
+	glFinish();
+	glPopMatrix();
+
+	position = scissor_2->GetPosition();
+	angle = scissor_2->GetAngle() * 180 / b2_pi;
+
+	glPushMatrix();
+	glTranslatef(position.x, position.y, 0.0f);
+	glRotatef(angle, 0.0f, 0.0f, 1.0f);
+	glColor3f(0.5f, 0.1f, 1.0f);
+
+	glBegin(GL_QUADS);
+	for (int i = 0; i < 4; i++) {
+		glVertex2f(scissor_box.m_vertices[i].x, scissor_box.m_vertices[i].y);
+	}
+	glEnd();
+	glFinish();
+	glPopMatrix();
+
+
+	/* (7) prismatic 장애물 그리기 */
+	position = prisma_1->GetPosition();
+	angle = prisma_1->GetAngle() * 180 / b2_pi;
+
+	glPushMatrix();
+	glTranslatef(position.x, position.y, 0.0f);
+	glRotatef(angle, 0.0f, 0.0f, 1.0f);
+	glColor3f(0.5f, 0.9f, 0.4f);
+
+	glBegin(GL_QUADS);
+	for (int i = 0; i < 4; i++) {
+		glVertex2f(prisma_box.m_vertices[i].x, prisma_box.m_vertices[i].y);
+	}
+	glEnd();
+	glFinish();
+	glPopMatrix();
+
+	position = prisma_2->GetPosition();
+	angle = prisma_2->GetAngle() * 180 / b2_pi;
+
+	glPushMatrix();
+	glTranslatef(position.x, position.y, 0.0f);
+	glRotatef(angle, 0.0f, 0.0f, 1.0f);
+	glColor3f(0.1f, 0.8f, 1.0f);
+
+	glBegin(GL_QUADS);
+	for (int i = 0; i < 4; i++) {
+		glVertex2f(prisma_box.m_vertices[i].x, prisma_box.m_vertices[i].y);
+	}
+	glEnd();
+	glFinish();
+	glPopMatrix();
+
+	glutSwapBuffers();
+
+	/* tmp ground */
+	position = tmp_gnd->GetPosition();
+	angle = tmp_gnd->GetAngle() * 180 / b2_pi;
+
+	glPushMatrix();
+	glTranslatef(position.x, position.y, 0.0f);
+	glRotatef(angle, 0.0f, 0.0f, 1.0f);
+	glColor3f(0.5f, 0.1f, 1.0f);
+
+	glBegin(GL_QUADS);
+	for (int i = 0; i < 4; i++) {
+		glVertex2f(tmp_box.m_vertices[i].x, tmp_box.m_vertices[i].y);
+	}
+	glEnd();
+	glFinish();
+	glPopMatrix();
+
+	position = tmp_gnd2->GetPosition();
+	angle = tmp_gnd2->GetAngle() * 180 / b2_pi;
+
+	glPushMatrix();
+	glTranslatef(position.x, position.y, 0.0f);
+	glRotatef(angle, 0.0f, 0.0f, 1.0f);
+	glColor3f(0.5f, 0.1f, 1.0f);
+
+	glBegin(GL_QUADS);
+	for (int i = 0; i < 4; i++) {
+		glVertex2f(tmp_box2.m_vertices[i].x, tmp_box2.m_vertices[i].y);
+	}
+	glEnd();
+	glFinish();
+	glPopMatrix();
 }
 
 void Update(int value)
@@ -229,14 +347,14 @@ void Setup()
 		jd.enableLimit = true;
 
 		// body B - the overhead object (=flipper)
-		jd.motorSpeed = 0.0f;
+		jd.motorSpeed = -30.0f;
 		jd.localAnchorA = p1;
 		jd.bodyB = leftFlipper;
 		jd.lowerAngle = -30.0f * b2_pi / 180.0f;
 		jd.upperAngle = 5.0f * b2_pi / 180.0f;
 		m_leftJoint = (b2RevoluteJoint*)world->CreateJoint(&jd);
 
-		jd.motorSpeed = 0.0f;
+		jd.motorSpeed = 30.0f;
 		jd.localAnchorA = p2;
 		jd.bodyB = rightFlipper;
 		jd.lowerAngle = -5.0f * b2_pi / 180.0f;
@@ -244,9 +362,66 @@ void Setup()
 		m_rightJoint = (b2RevoluteJoint*)world->CreateJoint(&jd);
 	}
 
-	// 발사대
+	// 바람개비 장애물
 	{
+		/* 기본적인 설정 부분 */
+		// step1 : define body - 위치 설정
+		b2Vec2 p1(-6.0f, 30.0f), p2(-3.0f, 30.0f);
 
+		// step2 : create body - 각 scissor별로 만들기
+		b2BodyDef bd;
+		bd.type = b2_dynamicBody;
+
+		bd.position = p1;
+		scissor_1 = world->CreateBody(&bd);
+
+		bd.position = p2;
+		scissor_2 = world->CreateBody(&bd);
+
+		// step3 : create shape	
+		scissor_box.SetAsBox(1.75f, 0.1f);
+
+		// step4 : create fixture - 기타 설정
+		b2FixtureDef fd;
+		fd.shape = &scissor_box;
+		fd.density = 1.0f;
+
+		b2FixtureDef fd2;
+		fd2.shape = &scissor_box;
+		fd2.density = 1.0f;
+
+		// Step5 : Attach shape to body with fixture
+		scissor_1->CreateFixture(&fd);
+		scissor_2->CreateFixture(&fd2);
+		
+		/* flipper를 ground에 고정시키기 위한 과정*/
+		// body A - the underneath object (=ground)
+		b2RevoluteJointDef jd;
+		jd.bodyA = ground;
+		jd.localAnchorB.SetZero();
+		jd.enableMotor = true;
+		jd.maxMotorTorque = 1000.0f;
+		
+		// body B - the overhead object (=scissor)
+		jd.motorSpeed = 30.0f;
+		jd.localAnchorA = p1;
+		jd.bodyB = scissor_1;
+
+		m_scissor = (b2RevoluteJoint*)world->CreateJoint(&jd);
+
+		// body A - the underneath object (=ground)
+		b2RevoluteJointDef jd2;
+		jd2.bodyA = ground;
+		jd2.localAnchorB.SetZero();
+		jd2.enableMotor = true;
+		jd2.maxMotorTorque = 1000.0f;
+
+		// body B - the overhead object (=scissor)
+		jd2.motorSpeed = -30.0f;
+		jd2.localAnchorA = p2;
+		jd2.bodyB = scissor_2;
+
+		m_scissor_2 = (b2RevoluteJoint*)world->CreateJoint(&jd2);
 	}
 
 	// 핀볼
@@ -254,7 +429,7 @@ void Setup()
 		// Step1 : define body - 위치
 		b2BodyDef ball;
 		ball.type = b2_dynamicBody;
-		ball.position.Set(0.0f, 30.0f);
+		ball.position.Set(15.0f, 1.0f);
 
 		// Step2 : create body
 		b2Body* body = world->CreateBody(&ball);
@@ -273,6 +448,90 @@ void Setup()
 		body->CreateFixture(&ballfd);		
 		pin_ball = body;
 	}
+
+	// prismatic 이용 장애물 
+	{	
+		/* 기본적인 설정 부분 */
+		// step1 : define body - 위치 설정
+		b2Vec2 p1(-2.0f, 16.0f), p2(2.0f, 17.0f);
+
+		// step2 : create body - 각 flipper별로 만들기
+		b2BodyDef bd;
+		bd.type = b2_dynamicBody;
+
+		bd.position = p1;
+		prisma_1 = world->CreateBody(&bd);
+
+		bd.position = p2;
+		prisma_2 = world->CreateBody(&bd);
+
+		// step3 : create shape	
+		prisma_box.SetAsBox(1.75f, 0.1f);
+
+		// step4 : create fixture - 기타 설정
+		b2FixtureDef fd;
+		fd.shape = &prisma_box;
+		fd.density = 1.0f;
+
+		// Step5 : Attach shape to body with fixture
+		prisma_1->CreateFixture(&fd);
+		prisma_2->CreateFixture(&fd);
+
+		/* flipper를 ground에 고정시키기 위한 과정*/
+		b2PrismaticJointDef jointDef;
+		b2Vec2 worldAxis(1.0f, 0.0f);
+	
+		jointDef.Initialize(prisma_1, prisma_2, prisma_1->GetWorldCenter(), worldAxis);
+		jointDef.lowerTranslation = -5.0f;
+		jointDef.upperTranslation = 2.5f;
+		jointDef.enableLimit = true;
+		jointDef.maxMotorForce = 10.0f;
+		jointDef.motorSpeed = 30.0f;
+		jointDef.enableMotor = true;
+
+		m_prisma = (b2PrismaticJoint*)world->CreateJoint(&jointDef);
+	}
+
+	// 임시 ground
+	{
+		b2Vec2 p1(-5.0f, 15.0f);
+
+		b2BodyDef bd;
+		bd.type = b2_staticBody;
+
+		bd.position = p1;
+		tmp_gnd = world->CreateBody(&bd);
+
+		tmp_box.SetAsBox(20.75f, 0.1f);
+
+		b2FixtureDef fd;
+		fd.shape = &tmp_box;
+		fd.density = 1.0f;
+		fd.friction = 0.0f;
+
+		tmp_gnd->CreateFixture(&fd);
+
+	}
+	// 임시 ground
+	{
+		b2Vec2 p1(-5.0f, 17.0f);
+
+		b2BodyDef bd;
+		bd.type = b2_staticBody;
+
+		bd.position = p1;
+		tmp_gnd2 = world->CreateBody(&bd);
+
+		tmp_box2.SetAsBox(20.75f, 0.1f);
+
+		b2FixtureDef fd;
+		fd.shape = &tmp_box2;
+		fd.density = 1.0f;
+		fd.friction = 0.0f;
+
+		tmp_gnd2->CreateFixture(&fd);
+
+	}
 }
 
 void DokeyBoard (unsigned char key, int x, int y) 
@@ -290,11 +549,13 @@ void DokeyBoard (unsigned char key, int x, int y)
 		break;
 	case 'a':
 		m_leftJoint->SetMotorSpeed(40.0f);
-		m_rightJoint->SetMotorSpeed(-40.0f);
+		m_rightJoint->SetMotorSpeed(-40.0f);	
 		break;
 	case 'd':
 		m_leftJoint->SetMotorSpeed(-40.0f);
 		m_rightJoint->SetMotorSpeed(40.0f);
+		break;
+	case 'x':		
 		break;
 	default:
 		break;
